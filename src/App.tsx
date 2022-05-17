b713f92bf842143c16392801136d180763f90097
farid95g <faridg.1995@gmail.com>
2022-05-17 11:17:49 +0400

    initial project layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Layout, Form, Input, Button, Row, Col } from 'antd'
+import './App.css'
+import 'antd/dist/antd.min.css'
 
-function App() {
+const { Header, Footer, Sider, Content } = Layout
+
+const App: React.FC = () => {
+  const onFinish = (values: any) => {
+    console.log('Success:', values)
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Layout>
+      <Header>
+        <div className='header'>
+          <h1>Notes Ant Design</h1>
+        </div>
+      </Header>
+
+      <Layout className='main'>
+        {/* <Sider></Sider> */}
+
+        <Content>
+          <Form
+            name="basic"
+            wrapperCol={{ span: 24 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+            layout='vertical'
+          >
+            <Row gutter={[16, 24]}>
+              <Col span={12}>
+                <Form.Item
+                  label="Title"
+                  name="title"
+                  rules={[{ required: true, message: 'Please enter note title!' }]}
+                >
+                  <Input placeholder='Note title' />
+                </Form.Item>
+              </Col>
+
+              <Col span={12}>
+                <Form.Item
+                  label="Content"
+                  name="content"
+                  rules={[{ required: true, message: 'Please enter ntoe content!' }]}
+                >
+                  <Input placeholder='Note content' />
+                </Form.Item>
+              </Col>
+            </Row>
+
+            <Form.Item wrapperCol={{ span: 24 }}>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </Content>
+      </Layout>
+
+      <Footer>
+        <div className='footer'>
+          <strong>&copy;Ant Design Footer</strong>
+          <strong>{new Date().getFullYear()}</strong>
+        </div>
+      </Footer>
+    </Layout>
+  )
 }
 
-export default App;
+export default App
