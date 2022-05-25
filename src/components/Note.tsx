cf94e29a8b47f51552db6c2858d5f9a441dfcf62
farid95g <faridg.1995@gmail.com>
2022-05-25 17:23:52 +0400

    popup confirmation for deleting note

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { Skeleton, Card, Avatar, Col } from 'antd'
+import { Skeleton, Card, Avatar, Col, Popconfirm, message } from 'antd'
 import { EyeOutlined, EditOutlined, DeleteOutlined } from '@ant-design/icons'
 import { INoteProps } from '../utils/interfaces/notes'
 import { NotesContext } from '../context/NotesContext/NotesContext'
@@ -7,6 +7,12 @@ import { ModalContext } from '../context/ModalContext/ModalContext'
 import { Modal } from '../utils/enums/modal'
 
 const { Meta } = Card
+
+const text = 'Are you sure to delete this task?'
+const confirm = (func: (id: string) => void, id: string) => {
+    message.success('Note Deleted.')
+    func(id)
+}
 
 export const Note: React.FC<INoteProps> = ({ 
     id, 
@@ -27,7 +33,15 @@ export const Note: React.FC<INoteProps> = ({
                         key='edit' 
                         onClick={toggleModal.bind(null, Modal.SHOW_MODAL, { id, title, content })}
                     />,
-                    <DeleteOutlined key='delete' onClick={removeNote.bind(null, id)} />
+                    <Popconfirm
+                        title={text}
+                        placement='bottom'
+                        okText='Yes'
+                        cancelText='No'
+                        onConfirm={confirm.bind(null, removeNote, id)}
+                    >
+                        <DeleteOutlined key='delete' />
+                    </Popconfirm>
                 ]}
             >
                 <Skeleton 
